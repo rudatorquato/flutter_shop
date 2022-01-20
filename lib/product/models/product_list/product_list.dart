e9187b75e70ba44407d0b5e553a1595c0d4cb5b8
Rudá Torquato <rudatorquato@gmail.com>
2022-01-20 15:19:31 -0300

    Filtrar Favoritos #01

--- lib/product/models/product_list/product_list.dart
+++ lib/product/models/product_list/product_list.dart
@@ -4,7 +4,24 @@ import 'package:shop/product/models/product/product.dart';
 
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
-  List<Product> get items => [..._items];
+  bool _showFavoriteOnly = false;
+
+  List<Product> get items {
+    if (_showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
+    return [..._items];
+  }
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
--- lib/product/widget/products_overview/products_overview_widget.dart
+++ lib/product/widget/products_overview/products_overview_widget.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/product/models/product_list/product_list.dart';
 import 'package:shop/product/widget/product_grid/product_grid.dart';
+
+enum FilterOptions {
+  favorite,
+  all,
+}
 
 class ProductOverviewWidget extends StatelessWidget {
   const ProductOverviewWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Center(
@@ -13,6 +21,28 @@ class ProductOverviewWidget extends StatelessWidget {
             'Minha Loja',
           ),
         ),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.favorite,
+              ),
+              const PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.all,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          )
+        ],
       ),
       body: const ProductGridWidget(),
     );
