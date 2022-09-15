72aa341b6dbe556a0ab2dba75d62d52b27b95d41
Rudá Torquato <rudatorquato@gmail.com>
2022-09-15 10:19:42 -0300

    Itens do Carrinho

--- lib/cart/widget/cart_item/cart_item_widget.dart
+++ lib/cart/widget/cart_item/cart_item_widget.dart
@@ -11,6 +11,33 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(cartItem.name);
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text(
+                  '${cartItem.price}',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryTextTheme.headline6?.color,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          title: Text(cartItem.name),
+          subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantify}'),
+          trailing: Text('${cartItem.quantify}x'),
+        ),
+      ),
+    );
   }
 }
