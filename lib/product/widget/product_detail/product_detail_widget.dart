64eb28011a72aa093eb80ba99ed54bd6ae4ebd92
Rudá Torquato <rudatorquato@gmail.com>
2022-09-16 14:13:20 -0300

    Componente Detalhe Produto

--- lib/product/widget/product_detail/product_detail_widget.dart
+++ lib/product/widget/product_detail/product_detail_widget.dart
@@ -14,6 +14,37 @@ class ProductDetailWidget extends StatelessWidget {
       appBar: AppBar(
         title: Center(child: Text(product.name)),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'R\$ ${product.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
