e54d6da9db870f60840619f102839733b495f481
Rudá Torquato <rudatorquato@gmail.com>
2022-09-19 10:08:13 -0300

    Componente Pedidos #01

---
+++ lib/order/widget/orders_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OrdersWidget extends StatelessWidget {
+  const OrdersWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Meus Pedidos')),
+    );
+  }
+}
